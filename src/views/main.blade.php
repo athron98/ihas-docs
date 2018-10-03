953b2cdc9626b7d75cb49edd0452c171f1228d85
Gilang Albathin Nurhabibi <athron.poster@gmail.com>
2018-10-04 02:38:09 +0700

    init

---
+++ src/views/main.blade.php
@@ -0,0 +1,107 @@
+
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="chrome=1">
+  <title>I18ns API documentation - iHAS</title>
+  <link rel="stylesheet" href="/static/css/normalize.css">
+  <link rel="stylesheet" href="/static/css/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--[if lt IE 9]>
+  <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+  <!--[if IE]>
+  <link rel="stylesheet" href="/static/css/for-ie-only.css">
+  <![endif]-->
+</head>
+<body>
+
+
+  <header class="header cf" role="banner">
+      			<div class="wrap">
+
+			<a href=""><h1><span>iHAS</span> Integrated Hospital Automation - API Documentation</h1></a>
+
+        	</a>
+        	</div>
+
+  </header>
+
+  <div class="wrap">
+    <div class="content">
+
+    <aside>
+
+<h1 class="page-title"><a href=".">I18Ns</a></h1>
+      <p class="intro">API Documentation.</p>
+    <nav>
+        <ul>
+          <li><a href="" class="overview">Overview</a></li>
+          <li><a href="" class="getting_started">Getting Started</a></li>
+          <li><a href="" class="console">API calls</a></li>
+          <li><a href="" class="fields">Field reference</a></li>
+        </ul>
+    </nav>
+
+    </aside>
+
+    <section class="main-content" role="main">
+      <h3 id="overview">Overview</h3>
+
+<p>The overview of the data provided and the API purpose of this section is not available yet.</p>
+
+<h5 id="using-the-api">Using the API</h5>
+<p>We built the API to be as self-documenting as possible, but if you find yourself overwhelmed, we organized this site into four major areas.</p>
+
+<ul>
+  <li><a href="">Getting started</a> introduces you to the operations offered by the API.</li>
+  <li><a href="">API calls</a> gives you a hands-on experience of those operations with an interactive console.</li>
+  <li><a href="">Field reference</a> lists and describes the type of information provided by the API.</li>
+</ul>
+
+<body id="overview"></body>
+
+    </section>
+
+    </div>
+  </div> <!--wrap-->
+
+    <footer class="footer cf" role="contentinfo">
+      <div class="wrap">
+
+            <section class="footer-oss">
+                <h4>Open Source AGPPL</h4>
+                <p>
+                    iHAS is copyright of Gilang Albathin Nurhabibi licensed under AGPPL
+                    <a href="https://athron98.xyz/AGPPL">
+                        View our full source code policy.
+                    </a>
+                </p>
+            </section>
+
+
+            <section class="footer-links">
+                <ul>
+                    <li>
+                        <a href="https://ihas.athron98.xyz/">ihas.athron98.xyz</a>
+                    </li>
+                    <li>
+                        <a href="https://developer.ihas.athron98.xyz">developer.ihas.athron98.xyz</a>
+                    </li>
+                    <li>
+                        <a href="https://athron98.github.io/ihas">athron98.github.io/ihas</a>
+                    </li>
+                </ul>
+            </section>
+
+
+
+
+      </div>
+    </footer>
+  <script src="/static/js/docs.min.js"></script>
+  <script type="text/javascript" src="/static/js/expandables.js"></script>
+</body>
+</html>
